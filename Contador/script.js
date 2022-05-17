7109995fa49af36634be300aee374caa673ab821
IgorPerrupato <ijperrupato@gmail.com>
2022-05-17 12:49:18 -0400

    contador e to do list

---
+++ Contador/script.js
@@ -0,0 +1,26 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if (currentNumber >= 0){
+        currentNumberWrapper.style = "color: orange"
+        
+    }
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if (currentNumber < 0){
+        currentNumberWrapper.style = "color: red"
+    }
+}
+
+
+
+
